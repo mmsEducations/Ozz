7b3038a358073570688c592ba6725436fdb55877
mmsmesut <m.mesutsincar@hotmail.com>
2024-08-25 13:04:04 +0300

    api response added

---
+++ Ozz.Core/ApiReponses/Response.cs
@@ -0,0 +1,11 @@
+﻿namespace Ozz.Core.ApiReponses
+{
+    public class Response<T>
+    {
+        public int StatusCode { get; set; } //İşlem status codu
+        public bool IsSuccess { get; set; } //İşlem durumunu 
+        public string Message { get; set; } //mesaj verme ve hata için kullanılır
+        public string Error { get; set; } //Hata mesajlarnını tutar 
+        public T Data { get; set; }
+    }
+}
