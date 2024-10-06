7ee06f3ee452a83024e8cd21ba2a28787b220720
mmsmesut <m.mesutsincar@hotmail.com>
2024-10-06 12:39:38 +0300

    policy role added.

--- Ozz.Core/Authorization/TokenService.cs
+++ Ozz.Core/Authorization/TokenService.cs
@@ -57,6 +57,53 @@ namespace Ozz.Core.Authorization
 
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
+
+        public string GenerateToken(string userId, string userEmail, List<string> roles)
+        {
+
+            //"JwtSettings": {
+            //"SecretKey": "LmN1kTe2vD8RV+h0tM5aKg0mRj+zLkQNL2LwDzY9M9g=", //Minimum 16 karakterli olmalı
+            //"Issuer": "https://localhost:7132", //Token'i oluştuan sunucu,Token'i kimin  oluşturudğunu belirler
+            //"Audience", //Token'ı kullanacak uygulama,site//Kullanan
+            //"ExpiryMinutes": 60
+            //}
+
+            //Header
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+            var secretKey = jwtSettings["SecretKey"];
+            var issuer = jwtSettings["Issuer"];
+            var audience = jwtSettings["Audience"];
+            var expireMinutes = int.Parse(jwtSettings["ExpiryMinutes"]);
+
+            //Payload->Claim
+
+            var claims = new List<Claim>
+            {
+                 new Claim(JwtRegisteredClaimNames.Sub,userId), //Token'ın hangi kullanıcıcya ait olduğunu  belirtmek için kullanılır
+                 new Claim(JwtRegisteredClaimNames.Email,userEmail), //Token'ın hangi epostaya ait olduğunu  belirtmek için kullanılır
+                 new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()) //Token'ı benzersiz yapan mekaznizma ,Her token için farklı bir Jti değeri oluşturur 
+            };
+
+            //Kullanıcı rollerini token'a bağla 
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));//İmza için kullanılacak key'i byte'a dönüştürüyrouz 
+            var signinCreds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);//ilgili key ve algoritmaya göre token oluşturulacaktır 
+
+            //Signature 
+            var token = new JwtSecurityToken(
+               issuer: issuer,
+               audience: audience,
+               claims: claims,
+               expires: DateTime.Now.AddMinutes(expireMinutes),
+               signingCredentials: signinCreds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
 
